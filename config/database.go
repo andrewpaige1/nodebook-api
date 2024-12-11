8e1a044dc05f8c188f15cab4dfbdff07db2b4daf
Andrew Paige <keeperpaige@gmail.com>
2024-12-10 20:11:33 -0500

    update env access

--- config/database.go
+++ config/database.go
@@ -1,9 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/andrewpaige1/nodebook-api/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,6 +13,13 @@ import (
 var Database *gorm.DB
 
 func Connect() error {
+	if os.Getenv("RENDER") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			fmt.Println("Warning: .env file not found, environment variables might not be loaded.")
+		}
+	}
+
 	var err error
 	dbURL := os.Getenv("DB_URL")
 	Database, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
