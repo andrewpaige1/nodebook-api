fe65e55b374162fc2f7321b5d6e18eb1133bff96
Andrew Paige <keeperpaige@gmail.com>
2024-12-16 22:52:33 -0500

    remove domain flag

--- handlers/user.go
+++ handlers/user.go
@@ -76,7 +76,6 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 			Value:    tokenString,
 			Path:     "/",
 			HttpOnly: true,
-			Domain:   ".mindthred.com",
 			Secure:   true, // Use only in HTTPS
 			SameSite: http.SameSiteLaxMode,
 			MaxAge:   86400, // 24 hours
@@ -118,7 +117,6 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 		Name:     "auth_token",
 		Value:    tokenString,
 		Path:     "/",
-		Domain:   ".mindthred.com",
 		HttpOnly: true,
 		Secure:   true, // Use only in HTTPS
 		SameSite: http.SameSiteLaxMode,
